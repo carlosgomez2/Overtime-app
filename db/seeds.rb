0b862f902e4271131c153906ea57e39ef935a75f
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-07-26 16:56:29 -0500

    updated seeds file and readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: "carlos@gmail.com", password: "123456789", password_confirmation: "123456789", first_name: "carlos", last_name: "gomez")
+
+puts "1 User created"
+
 100.times do |posts|
-  Post.create!(date: Date.today, rationale: "#{posts} rationale content")
+  Post.create!(date: Date.today, rationale: "#{posts} rationale content", user_id: @user.id)
 end
 
 puts "** 100 post has been created succesfully! **"
