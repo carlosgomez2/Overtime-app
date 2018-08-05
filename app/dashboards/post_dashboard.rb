6852007aebe7892925ac64d3030c33dfed9d59bf
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-08-05 15:31:26 -0500

    status shows on admin dashboard, updated seeds for create admin user

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -14,6 +14,7 @@ class PostDashboard < Administrate::BaseDashboard
     rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: true),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -22,9 +23,10 @@ class PostDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :user,
     :id,
-    :date,
+    :status,
+    :user,
+    # :date,
     :rationale,
   ].freeze
 
@@ -32,6 +34,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
     :rationale,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-@user = User.create(email: "carlos@gmail.com", password: "123456789", password_confirmation: "123456789", first_name: "carlos", last_name: "gomez")
+@user = User.create(email: "carlos@gmail.com", password: "123456789", password_confirmation: "123456789", first_name: "Carlos", last_name: "Gomez")
 
 puts "1 User created"
+
+AdminUser.create(email: "admin@gmail.com", password: "adminpassword", password_confirmation: "adminpassword", first_name: "Jonh", last_name: "Snow")
+
+puts "1 Admin User created"
 
 100.times do |posts|
   Post.create!(date: Date.today, rationale: "#{posts} rationale content", user_id: @user.id)
