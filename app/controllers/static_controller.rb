6c289e123d70f228cb310e3a82ddeafd1f3f6b87
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-08-16 18:21:49 -0500

    implemented data into for admin review

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,5 @@
 class StaticController < ApplicationController
-  
+  def homepage
+    @pending_approvals = Post.where(status: 'submitted')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ AdminUser.create(email: "admin@gmail.com", password: "adminpassword", password_c
 puts "1 Admin User created"
 
 100.times do |posts|
-  Post.create!(date: Date.today, rationale: "#{posts} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "Pour-over XOXO swag post-ironic cray snackwave PBR&B hammock iPhone fashion axe fanny pack jean shorts knausgaard. Before they sold out snackwave vape, food truck cold-pressed salvia authentic meh. Hot chicken next level before they sold out kogi helvetica cardigan single-origin coffee shaman af godard chicharrones adaptogen tumeric vaporware. YOLO butcher shoreditch pabst vexillologist. Man braid shaman organic lomo.", user_id: @user.id, overtime_request: 2.5)
 end
 
 100.times do |audit_log|
