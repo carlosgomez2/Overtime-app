2e3313b3caf1ad0ca7989bc21041487bca45862d
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-08-27 11:55:06 -0500

    updated precedence on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,14 @@ AdminUser.create(email: "admin@gmail.com", password: "adminpassword", password_c
 
 puts "1 Admin User created"
 
-30.times do |posts|
-  Post.create!(date: Date.today, rationale: "Pour-over XOXO swag post-ironic cray snackwave PBR&B hammock iPhone fashion axe fanny pack jean shorts knausgaard. Before they sold out snackwave vape, food truck cold-pressed salvia authentic meh. Hot chicken next level before they sold out kogi helvetica cardigan single-origin coffee shaman af godard chicharrones adaptogen tumeric vaporware. YOLO butcher shoreditch pabst vexillologist. Man braid shaman organic lomo.", user_id: @user.id, overtime_request: 2.5)
-end
-
 6.times do |audit_log|
   AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - SecureRandom.random_number(1..60).days))
 end
 
-puts "** 30 posts has been created succesfully! **"
 puts "** 30 audit logs has been created succesfully! **"
+
+30.times do |posts|
+  Post.create!(date: Date.today, rationale: "Pour-over XOXO swag post-ironic cray snackwave PBR&B hammock iPhone fashion axe fanny pack jean shorts knausgaard. Before they sold out snackwave vape, food truck cold-pressed salvia authentic meh. Hot chicken next level before they sold out kogi helvetica cardigan single-origin coffee shaman af godard chicharrones adaptogen tumeric vaporware. YOLO butcher shoreditch pabst vexillologist. Man braid shaman organic lomo.", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "** 30 posts has been created succesfully! **"
